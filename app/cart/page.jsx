d9f2b0af5f6bdf1f1f7d9e3dc13df88fc6d27e2f
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-05-19 15:23:31 +0700

    display data in cart page

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,45 +1,46 @@
-import Image from "next/image"
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { useStore } from "@/lib/store";
 
 export default function CartPage() {
-    return (
-        <div className="bg-gray-100">
-            <div className="wrapper py-11">
-                <div className="relative max-w-[700px] mx-auto">
-                    <h4 className="text-3xl font-bold mb-5">Keranjang</h4>
-                    <div className="bg-white rounded-lg p-5">
-                        <div className="flex flex-col gap-4">
-                            <div className="flex justify-between w-full">
-                                <div className="flex gap-4">
-                                    <div className="max-w-[100px]">
-                                        <Image src="/images/no-image-square.jpg" width={500} height={500} alt="no image" />
-                                    </div>
-                                    <div className="relative text-lg ">
-                                        Mens Casual Premium Slim Fit T-Shirts
-                                    </div>
-                                </div>
-                                <div className="relative font-bold">$22.3</div>
-                            </div>
-                            <div className="flex justify-between w-full">
-                                <div className="flex gap-4">
-                                    <div className="max-w-[100px]">
-                                        <Image src="/images/no-image-square.jpg" width={500} height={500} alt="no image" />
-                                    </div>
-                                    <div className="relative text-lg ">
-                                        Mens Casual Premium Slim Fit T-Shirts
-                                    </div>
-                                </div>
-                                <div className="relative font-bold">$22.3</div>
-                            </div>
-                        </div>
-                        <div className="flex justify-between mt-10 text-3xl font-bold">
-                            <div>Total</div>
-                            <div>$44.6</div>
-                        </div>
-                        <div className="btn mt-5 p-3 cursor-pointer">Checkout</div>
+  const { cart } = useStore();
+  return (
+    <div className="bg-gray-100">
+      <div className="wrapper py-11">
+        <div className="relative max-w-[700px] mx-auto">
+          <h4 className="text-3xl font-bold mb-5">Keranjang</h4>
+          <div className="bg-white rounded-lg p-5">
+            {cart.length > 0 ? cart.map((item) => (
+              <div className="my-14 flex flex-col gap-4" id={item.id}>
+                <div className="flex justify-between w-full">
+                  <div className="flex gap-4">
+                    <div className="max-w-[100px]">
+                      <Image
+                        src={item.img}
+                        width={500}
+                        height={500}
+                        alt="no image"
+                      />
                     </div>
+                    <div className="relative text-lg flex flex-col justify-between">
+                      <div>{item.name}</div>
+                      <b className="flex justify-end">( X {item.total} )</b>
+                    </div>
+                  </div>
+                  <div className="relative font-bold">{item.price}</div>
                 </div>
-            </div>
+                <div className="flex justify-between mt-10 text-3xl font-bold">
+                  <div>Total</div>
+                  <div>$ {item.price * item.total}</div>
+                </div>
+              </div>
+            )) : <div className="text-white font-extrabold text-[15px] h-[200px] bg-blue-500 flex justify-center items-center rounded-lg">Upss..Looks like you haven't added any items yet</div>}
+            <div className="btn mt-5 p-3 cursor-pointer">Checkout</div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,34 +1,40 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import Link from "next/link";
 import { IoMdCart } from "react-icons/io";
 import { useStore } from "@/lib/store";
 import { CiMenuKebab } from "react-icons/ci";
 
-
 export default function Header() {
-    const { cart, productName, price } = useStore()
-
-    console.log(cart.length)
-    return (
-        <div className="bg-color-main">
-            <div className="wrapper">
-                <div className="flex justify-between items-center py-5">
-                    <Link href="/" className="text-white text-2xl font-extrabold">MyCommerce</Link>
-                    <div className="hidden lg:block">
-                        <input type="text" placeholder="Search..." className="py-2 px-3 rounded-lg min-w-[300px]" />
-                    </div>
-                    <div className="flex">
-                        <Link href="/cart" className="text-3xl text-white relative">
-                            <IoMdCart />
-                            <div className="absolute text-[10px] top-0 right-0 bg-black text-white w-5 h-5 rounded-full flex items-center justify-center">{cart.length}</div>
-                        </Link>
-                        <div className="text-white text-2xl lg:hidden block">
-                            <CiMenuKebab />
-                        </div>
-                    </div>
-                </div>
+  const { cart, productName, price } = useStore();
+  console.log(cart.length);
+  return (
+    <div className="bg-color-main">
+      <div className="wrapper">
+        <div className="flex justify-between items-center py-5">
+          <Link href="/" className="text-white text-2xl font-extrabold">
+            MyCommerce
+          </Link>
+          <div className="hidden lg:block">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="py-2 px-3 rounded-lg min-w-[300px]"
+            />
+          </div>
+          <div className="flex">
+            <Link href="/cart" className="text-3xl text-white relative">
+              <IoMdCart />
+              <div className="absolute text-[10px] top-0 right-0 bg-black text-white w-5 h-5 rounded-full flex items-center justify-center">
+                {cart.length}
+              </div>
+            </Link>
+            <div className="text-white text-2xl lg:hidden block">
+              <CiMenuKebab />
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
