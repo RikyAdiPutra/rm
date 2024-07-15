ce4bea0aff2d9fd05ab0355114c72645c6384c14
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-07-15 14:13:23 +0700

    resolve responsive product detail

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -8,7 +8,7 @@ import Basecontent from "@/components/basecontent";
 import { useStore } from "@/lib/store";
 
 function DataPageDetail() {
-  const [cartValue, setCartValue] = useState(2);
+  const [cartValue, setCartValue] = useState(0);
   const { cart, updateCart, test } = useStore();
 
   console.log(cart);
@@ -35,29 +35,31 @@ function DataPageDetail() {
   const dataContent = query.data?.data;
   return (
     <div className="wrapper py-11">
-      <div className="flex">
-        <div className="w-6/12">
-          {dataContent?.image ? (
-            <Image src={`${dataContent?.image}`} width={500} height={500} />
-          ) : (
-            <Image src="/images/no-image-square.jpg" width={500} height={500} />
-          )}
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex justify-center lg:justify-start">
+          <div className="w-6/12">
+            {dataContent?.image ? (
+              <Image src={`${dataContent?.image}`} width={500} height={500} />
+            ) : (
+              <Image src="/images/no-image-square.jpg" width={500} height={500} />
+            )}
+          </div>
         </div>
-        <div className="w-6/12">
-          <h1 className="text-4xl font-bold text-color-main">
+        <div className="w-full lg:w-6/12 mt-2 lg:mt-0">
+          <h1 className="text-2xl lg:text-4xl text-center lg:text-start font-bold text-gray-500 lg:text-color-main">
             {dataContent?.title}
           </h1>
-          <h5 className="my-3">Category: {dataContent?.category}</h5>
+          <h5 className="my-3 font-bold">Category: {dataContent?.category}</h5>
           <h4 className="my-4 text-2xl font-bold">$ {dataContent?.price}</h4>
           <h5 className="text-lg font-bold">Description:</h5>
           <p>
             Your perfect pack for everyday use and walks in the forest. Stash
             your laptop (up to 15 inches) in the padded sleeve, your everyday
           </p>
-          <div className="my-5">Rating</div>
-          <div className="flex gap-3 items-center">
+          {/* <div className="my-5">Rating</div> */}
+          <div className="flex gap-3 items-center mt-8 lg:mt-2 justify-center lg:justify-start">
             <div
-              className="relative btn cursor-pointer"
+              className="relative btn cursor-pointer w-40 lg:w-0"
               onClick={() => {
                 if (cartValue > 0) {
                   setCartValue(cartValue - 1);
@@ -68,7 +70,7 @@ function DataPageDetail() {
             </div>
             <div>{cartValue}</div>
             <div
-              className="relative btn cursor-pointer"
+              className="relative btn cursor-pointer w-40 lg:w-0"
               onClick={() => setCartValue(cartValue + 1)}
             >
               +
